a74af60daa2388e6a108810568a1be1c8bf7a142
pdh903 <pdh90345@gmail.com>
2024-06-21 20:52:03 +0900

    commit test

--- src/main/java/com/jpabook/jpashop/service/OrderService.java
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -34,6 +34,7 @@ public class OrderService {
         // 배송정보 생성
         Delivery delivery = new Delivery();
         delivery.setAddress(member.getAddress());
+        System.out.println("git test");
 
         // 주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
